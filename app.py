3643be9fdcf109d05a83402c74df361135d8fa27
Jonah123456789 <jonah@jo-dev.net>
2023-07-08 23:11:08 +0200

    First To-Do frontend

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/overview/")
+def overview():
     return render_template('overview.html')
+
+@app.route("/todos/")
+def todos():
+    return render_template('todos.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
