24ac19376294502a4f2cc8f22de89a222db3461d
Jonah123456789 <jonah@jo-dev.net>
2023-09-01 13:56:09 +0200

    Major rework of structure + preperation for auth

---
+++ app/auth.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template
+from . import db
+
+auth = Blueprint("auth", __name__)
+
+@auth.route("/login/")
+def login():
+    return render_template("")
+
+@auth.route("/signup/")
+def signup():
+    return "Signup"
+
+@auth.route("/logout/")
+def logout():
+    return "Logout"
---
+++ app/main.py
@@ -0,0 +1,19 @@
+from flask import render_template, redirect, Blueprint
+from . import db
+main = Blueprint("main", __name__)
+
+@main.route("/")
+def home():
+    return redirect("/overview")
+
+@main.route("/overview/")
+def overview():
+    return render_template('overview.html')
+
+@main.route("/todos/")
+def todos():
+    return render_template('todos.html')
+
+@main.route("/calendar/")
+def calendar():
+    return render_template('calendar.html')
